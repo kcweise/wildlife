0eff2c054d6a7b5edddf326305b051e39ddbadca
Keenan Weise <kcweise@gmail.com>
2024-05-17 15:32:02 -0500

    configured procfile and honcho/gunicorn

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,7 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
+
 export default defineConfig({
   plugins: [react()],
-})
+  root: './client', // Specify the root directory
+  build: {
+    outDir: './dist', // Specify the output directory
+  },
+});
