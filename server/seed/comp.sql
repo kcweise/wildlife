9713a4ab46e924f307536813c294dbc12dda20d7
Keenan Weise <kcweise@gmail.com>
2024-05-17 13:04:28 -0500

    Data in database, .sql files good

--- server/seed/comp.sql
+++ server/seed/comp.sql
@@ -1,10 +1,10 @@
-INSERT INTO Competitions (name, start_date, end_date, description, winner_id) VALUES 
+INSERT INTO Competitions (name, start_date, end_date, description) VALUES 
 
-('Best Overall Photo!', 2024-05-15 00:00:00, 2024-05-22 00:00:00, 'One week competition for best overall animal picture, enter, vote, win!', '') 
-('WHOA! TOO CLOSE', 2024-05-17 00:00:00, 2024-05-24 00:00:00, 'One week competition for best animal picture that is way too close, enter, vote, win!', '')
-('Cutest Picture', 2024-05-19 00:00:00, 2024-05-26 00:00:00, 'One week competition for cutest animal picture, enter, vote, win!', '')
-('Rarest Animal', 2024-05-22 00:00:00, 2024-05-29 00:00:00, 'One week competition for hard to capture or rarest animals, enter, vote, win!', '')
-('Best Overall Photo!', 2024-05-24 00:00:00, 2024-05-31 00:00:00, 'One week competition for best overall animal picture, enter, vote, win!', '')
-('WHOA! TOO CLOSE', 2024-05-26 00:00:00, 2024-06-02 00:00:00, 'One week competition for best animal picture that is way too close, enter, vote, win!', '')
-('Cutest Picture', 2024-05-28 00:00:00, 2024-06-04 00:00:00, 'One week competition for cutest animal picture, enter, vote, win!', '')
-('Rarest Animal', 2024-05-22 00:00:00, 2024-05-29 00:00:00, 'One week competition for hard to capture or rarest animals, enter, vote, win!', '')
+('Best Overall Photo!', '2024-05-15 00:00:00', '2024-05-22 00:00:00', 'One week competition for best overall animal picture, enter, vote, win!'), 
+('WHOA! TOO CLOSE', '2024-05-17 00:00:00', '2024-05-24 00:00:00', 'One week competition for best animal picture that is way too close, enter, vote, win!'),
+('Cutest Picture', '2024-05-19 00:00:00', '2024-05-26 00:00:00', 'One week competition for cutest animal picture, enter, vote, win!'),
+('Rarest Animal', '2024-05-22 00:00:00', '2024-05-29 00:00:00', 'One week competition for hard to capture or rarest animals, enter, vote, win!'),
+('Best Overall Photo!', '2024-05-24 00:00:00', '2024-05-31 00:00:00', 'One week competition for best overall animal picture, enter, vote, win!'),
+('WHOA! TOO CLOSE', '2024-05-26 00:00:00', '2024-06-02 00:00:00', 'One week competition for best animal picture that is way too close, enter, vote, win!'),
+('Cutest Picture', '2024-05-28 00:00:00', '2024-06-04 00:00:00', 'One week competition for cutest animal picture, enter, vote, win!'),
+('Rarest Animal', '2024-05-22 00:00:00', '2024-05-29 00:00:00', 'One week competition for hard to capture or rarest animals, enter, vote, win!')
--- server/seed/photos.sql
+++ server/seed/photos.sql
@@ -1,90 +1,90 @@
-INSERT INTO Photos (date_time, title, animal, description, photo_url, user_id) VALUES 
+INSERT INTO Photos (post_date_time, taken_date_time, title, animal, description, photo_url, user_id) VALUES 
 
-('2021-07-13 12:45:36', '', '', '', 'client/photos/animal_pic_001.jpg', 9)
-('2021-06-28 05:52:19', '', '', '', 'client/photos/animal_pic_002.jpg', 4)
-('2021-10-04 23:21:08', '', '', '', 'client/photos/animal_pic_003.jpg', 3)
-('2022-05-19 16:30:11', '', '', '', 'client/photos/animal_pic_004.jpg', 7)
-('2023-02-26 03:14:35', '', '', '', 'client/photos/animal_pic_005.jpg', 1)
-('2021-09-18 09:02:57', '', '', '', 'client/photos/animal_pic_006.jpg', 10)
-('2023-01-11 18:48:13', '', '', '', 'client/photos/animal_pic_007.jpg', 1)
-('2021-11-22 07:45:27', '', '', '', 'client/photos/animal_pic_008.jpg', 1)
-('2023-04-14 12:53:40', '', '', '', 'client/photos/animal_pic_009.jpg', 10)
-('2022-08-09 20:18:03', '', '', '', 'client/photos/animal_pic_010.jpg', 7)
-('2022-03-02 21:25:42', '', '', '', 'client/photos/animal_pic_011.jpg', 1)
-('2021-04-15 10:09:17', '', '', '', 'client/photos/animal_pic_012.jpg', 9)
-('2022-07-05 04:17:59', '', '', '', 'client/photos/animal_pic_013.jpg', 6)
-('2022-11-23 13:46:32', '', '', '', 'client/photos/animal_pic_014.jpg', 10)
-('2022-05-30 08:28:46', '', '', '', 'client/photos/animal_pic_015.jpg', 4)
-('2021-02-10 11:39:56', '', '', '', 'client/photos/animal_pic_016.jpg', 3)
-('2023-05-01 15:06:48', '', '', '', 'client/photos/animal_pic_017.jpg', 2)
-('2021-12-08 22:15:33', '', '', '', 'client/photos/animal_pic_018.jpg', 4)
-('2021-05-06 06:43:29', '', '', '', 'client/photos/animal_pic_019.jpg', 10)
-('2022-09-21 17:34:27', '', '', '', 'client/photos/animal_pic_020.jpg', 10)
-('2023-03-19 23:57:59', '', '', '', 'client/photos/animal_pic_021.jpg', 10)
-('2022-12-17 09:02:12', '', '', '', 'client/photos/animal_pic_022.jpg', 9)
-('2021-06-10 19:32:23', '', '', '', 'client/photos/animal_pic_023.jpg', 4)
-('2021-08-25 03:11:38', '', '', '', 'client/photos/animal_pic_024.jpg', 7)
-('2023-03-28 08:24:56', '', '', '', 'client/photos/animal_pic_025.jpg', 7)
-('2021-11-10 14:39:43', '', '', '', 'client/photos/animal_pic_026.jpg', 10)
-('2022-01-29 11:08:04', '', '', '', 'client/photos/animal_pic_027.jpg', 5)
-('2022-10-30 22:20:47', '', '', '', 'client/photos/animal_pic_028.jpg', 2)
-('2022-06-03 07:47:52', '', '', '', 'client/photos/animal_pic_029.jpg', 9)
-('2021-07-27 20:34:55', '', '', '', 'client/photos/animal_pic_030.jpg', 1)
-('2023-01-22 16:53:01', '', '', '', 'client/photos/animal_pic_031.jpg', 6)
-('2021-04-22 23:11:20', '', '', '', 'client/photos/animal_pic_032.jpg', 5)
-('2022-08-18 18:28:14', '', '', '', 'client/photos/animal_pic_033.jpg', 2)
-('2023-02-13 05:07:08', '', '', '', 'client/photos/animal_pic_034.jpg', 5)
-('2021-03-07 15:40:50', '', '', '', 'client/photos/animal_pic_035.jpg', 5)
-('2022-03-29 06:59:16', '', '', '', 'client/photos/animal_pic_036.jpg', 8)
-('2022-11-11 02:04:21', '', '', '', 'client/photos/animal_pic_037.jpg', 6)
-('2022-06-26 10:55:37', '', '', '', 'client/photos/animal_pic_038.jpg', 9)
-('2021-09-03 12:13:49', '', '', '', 'client/photos/animal_pic_039.jpg', 9)
-('2023-03-06 04:48:51', '', '', '', 'client/photos/animal_pic_040.jpg', 8)
-('2022-01-12 19:15:44', '', '', '', 'client/photos/animal_pic_041.jpg', 8)
-('2022-09-04 22:36:20', '', '', '', 'client/photos/animal_pic_042.jpg', 10)
-('2022-05-09 05:26:15', '', '', '', 'client/photos/animal_pic_043.jpg', 9)
-('2021-07-19 09:31:04', '', '', '', 'client/photos/animal_pic_044.jpg', 5)
-('2023-04-08 17:40:22', '', '', '', 'client/photos/animal_pic_045.jpg', 9)
-('2021-02-24 20:53:07', '', '', '', 'client/photos/animal_pic_046.jpg', 6)
-('2021-10-16 07:45:54', '', '', '', 'client/photos/animal_pic_047.jpg', 4)
-('2022-07-14 14:12:39', '', '', '', 'client/photos/animal_pic_048.jpg', 1)
-('2023-02-01 08:21:16', '', '', '', 'client/photos/animal_pic_049.jpg', 1)
-('2021-11-29 16:37:45', '', '', '', 'client/photos/animal_pic_050.jpg', 1)
-('2022-04-04 12:06:02', '', '', '', 'client/photos/animal_pic_051.jpg', 7)
-('2022-10-03 18:55:32', '', '', '', 'client/photos/animal_pic_052.jpg', 5)
-('2023-01-02 23:40:50', '', '', '', 'client/photos/animal_pic_053.jpg', 6)
-('2021-08-12 05:48:36', '', '', '', 'client/photos/animal_pic_054.jpg', 3)
-('2023-03-23 13:22:31', '', '', '', 'client/photos/animal_pic_055.jpg', 1)
-('2021-05-18 11:32:59', '', '', '', 'client/photos/animal_pic_056.jpg', 8)
-('2022-02-25 02:41:28', '', '', '', 'client/photos/animal_pic_057.jpg', 8)
-('2022-09-15 06:15:39', '', '', '', 'client/photos/animal_pic_058.jpg', 5)
-('2022-06-15 20:51:03', '', '', '', 'client/photos/animal_pic_059.jpg', 5)
-('2021-04-10 18:08:27', '', '', '', 'client/photos/animal_pic_060.jpg', 4)
-('2023-04-26 09:45:54', '', '', '', 'client/photos/animal_pic_061.jpg', 1)
-('2021-09-27 04:18:05', '', '', '', 'client/photos/animal_pic_062.jpg', 7)
-('2022-07-29 07:29:43', '', '', '', 'client/photos/animal_pic_063.jpg', 1)
-('2022-11-02 15:33:16', '', '', '', 'client/photos/animal_pic_064.jpg', 2)
-('2022-03-11 22:45:59', '', '', '', 'client/photos/animal_pic_065.jpg', 5)
-('2021-06-01 10:29:40', '', '', '', 'client/photos/animal_pic_066.jpg', 3)
-('2023-02-22 20:08:45', '', '', '', 'client/photos/animal_pic_067.jpg', 1)
-('2021-12-18 03:05:28', '', '', '', 'client/photos/animal_pic_068.jpg', 2)
-('2022-04-25 09:14:32', '', '', '', 'client/photos/animal_pic_069.jpg', 6)
-('2022-10-19 19:37:17', '', '', '', 'client/photos/animal_pic_070.jpg', 5)
-('2021-08-30 16:48:02', '', '', '', 'client/photos/animal_pic_071.jpg', 3)
-('2023-01-15 11:53:18', '', '', '', 'client/photos/animal_pic_072.jpg', 2)
-('2021-03-16 21:06:51', '', '', '', 'client/photos/animal_pic_073.jpg', 6)
-('2022-02-07 06:24:10', '', '', '', 'client/photos/animal_pic_074.jpg', 2)
-('2022-08-23 11:15:38', '', '', '', 'client/photos/animal_pic_075.jpg', 3)
-('2021-05-11 13:58:46', '', '', '', 'client/photos/animal_pic_076.jpg', 6)
-('2023-04-03 02:27:09', '', '', '', 'client/photos/animal_pic_077.jpg', 3)
-('2022-01-21 12:44:07', '', '', '', 'client/photos/animal_pic_078.jpg', 8)
-('2021-07-02 08:34:29', '', '', '', 'client/photos/animal_pic_079.jpg', 6)
-('2022-12-03 04:47:51', '', '', '', 'client/photos/animal_pic_080.jpg', 10)
-('2021-10-08 17:39:34', '', '', '', 'client/photos/animal_pic_081.jpg', 1)
-('2022-06-21 03:56:59', '', '', '', 'client/photos/animal_pic_082.jpg', 2)
-('2023-03-14 10:29:37', '', '', '', 'client/photos/animal_pic_083.jpg', 3)
-('2021-12-29 01:22:44', '', '', '', 'client/photos/animal_pic_084.jpg', 7)
-('2021-08-03 22:19:53', '', '', '', 'client/photos/animal_pic_085.jpg', 9)
-('2022-04-15 15:53:25', '', '', '', 'client/photos/animal_pic_086.jpg', 9)
-('2022-11-30 18:03:48', '', '', '', 'client/photos/animal_pic_087.jpg', 4)
-('2022-05-26 07:40:55', '', '', '', 'client/photos/animal_pic_088.jpg', 2)
+('2021-07-13 12:45:36', '', '', '', '', 'client/photos/animal_pic_001.jpg', 9),
+('2021-06-28 05:52:19', '', '', '', '', 'client/photos/animal_pic_002.jpg', 4),
+('2021-10-04 23:21:08', '', '', '', '', 'client/photos/animal_pic_003.jpg', 3),
+('2022-05-19 16:30:11', '', '', '', '', 'client/photos/animal_pic_004.jpg', 7),
+('2023-02-26 03:14:35', '', '', '', '', 'client/photos/animal_pic_005.jpg', 1),
+('2021-09-18 09:02:57', '', '', '', '', 'client/photos/animal_pic_006.jpg', 10),
+('2023-01-11 18:48:13', '', '', '', '', 'client/photos/animal_pic_007.jpg', 1),
+('2021-11-22 07:45:27', '', '', '', '', 'client/photos/animal_pic_008.jpg', 1),
+('2023-04-14 12:53:40', '', '', '', '', 'client/photos/animal_pic_009.jpg', 10),
+('2022-08-09 20:18:03', '', '', '', '', 'client/photos/animal_pic_010.jpg', 7),
+('2022-03-02 21:25:42', '', '', '', '', 'client/photos/animal_pic_011.jpg', 1),
+('2021-04-15 10:09:17', '', '', '', '', 'client/photos/animal_pic_012.jpg', 9),
+('2022-07-05 04:17:59', '', '', '', '', 'client/photos/animal_pic_013.jpg', 6),
+('2022-11-23 13:46:32', '', '', '', '', 'client/photos/animal_pic_014.jpg', 10),
+('2022-05-30 08:28:46', '', '', '', '', 'client/photos/animal_pic_015.jpg', 4),
+('2021-02-10 11:39:56', '', '', '', '', 'client/photos/animal_pic_016.jpg', 3),
+('2023-05-01 15:06:48', '', '', '', '', 'client/photos/animal_pic_017.jpg', 2),
+('2021-12-08 22:15:33', '', '', '', '', 'client/photos/animal_pic_018.jpg', 4),
+('2021-05-06 06:43:29', '', '', '', '', 'client/photos/animal_pic_019.jpg', 10),
+('2022-09-21 17:34:27', '', '', '', '', 'client/photos/animal_pic_020.jpg', 10),
+('2023-03-19 23:57:59', '', '', '', '', 'client/photos/animal_pic_021.jpg', 10),
+('2022-12-17 09:02:12', '', '', '', '', 'client/photos/animal_pic_022.jpg', 9),
+('2021-06-10 19:32:23', '', '', '', '', 'client/photos/animal_pic_023.jpg', 4),
+('2021-08-25 03:11:38', '', '', '', '', 'client/photos/animal_pic_024.jpg', 7),
+('2023-03-28 08:24:56', '', '', '', '', 'client/photos/animal_pic_025.jpg', 7),
+('2021-11-10 14:39:43', '', '', '', '', 'client/photos/animal_pic_026.jpg', 10),
+('2022-01-29 11:08:04', '', '', '', '', 'client/photos/animal_pic_027.jpg', 5),
+('2022-10-30 22:20:47', '', '', '', '', 'client/photos/animal_pic_028.jpg', 2),
+('2022-06-03 07:47:52', '', '', '', '', 'client/photos/animal_pic_029.jpg', 9),
+('2021-07-27 20:34:55', '', '', '', '', 'client/photos/animal_pic_030.jpg', 1),
+('2023-01-22 16:53:01', '', '', '', '', 'client/photos/animal_pic_031.jpg', 6),
+('2021-04-22 23:11:20', '', '', '', '', 'client/photos/animal_pic_032.jpg', 5),
+('2022-08-18 18:28:14', '', '', '', '', 'client/photos/animal_pic_033.jpg', 2),
+('2023-02-13 05:07:08', '', '', '', '', 'client/photos/animal_pic_034.jpg', 5),
+('2021-03-07 15:40:50', '', '', '', '', 'client/photos/animal_pic_035.jpg', 5),
+('2022-03-29 06:59:16', '', '', '', '', 'client/photos/animal_pic_036.jpg', 8),
+('2022-11-11 02:04:21', '', '', '', '', 'client/photos/animal_pic_037.jpg', 6),
+('2022-06-26 10:55:37', '', '', '', '', 'client/photos/animal_pic_038.jpg', 9),
+('2021-09-03 12:13:49', '', '', '', '', 'client/photos/animal_pic_039.jpg', 9),
+('2023-03-06 04:48:51', '', '', '', '', 'client/photos/animal_pic_040.jpg', 8),
+('2022-01-12 19:15:44', '', '', '', '', 'client/photos/animal_pic_041.jpg', 8),
+('2022-09-04 22:36:20', '', '', '', '', 'client/photos/animal_pic_042.jpg', 10),
+('2022-05-09 05:26:15', '', '', '', '', 'client/photos/animal_pic_043.jpg', 9),
+('2021-07-19 09:31:04', '', '', '', '', 'client/photos/animal_pic_044.jpg', 5),
+('2023-04-08 17:40:22', '', '', '', '', 'client/photos/animal_pic_045.jpg', 9),
+('2021-02-24 20:53:07', '', '', '', '', 'client/photos/animal_pic_046.jpg', 6),
+('2021-10-16 07:45:54', '', '', '', '', 'client/photos/animal_pic_047.jpg', 4),
+('2022-07-14 14:12:39', '', '', '', '', 'client/photos/animal_pic_048.jpg', 1),
+('2023-02-01 08:21:16', '', '', '', '', 'client/photos/animal_pic_049.jpg', 1),
+('2021-11-29 16:37:45', '', '', '', '', 'client/photos/animal_pic_050.jpg', 1),
+('2022-04-04 12:06:02', '', '', '', '', 'client/photos/animal_pic_051.jpg', 7),
+('2022-10-03 18:55:32', '', '', '', '', 'client/photos/animal_pic_052.jpg', 5),
+('2023-01-02 23:40:50', '', '', '', '', 'client/photos/animal_pic_053.jpg', 6),
+('2021-08-12 05:48:36', '', '', '', '', 'client/photos/animal_pic_054.jpg', 3),
+('2023-03-23 13:22:31', '', '', '', '', 'client/photos/animal_pic_055.jpg', 1),
+('2021-05-18 11:32:59', '', '', '', '', 'client/photos/animal_pic_056.jpg', 8),
+('2022-02-25 02:41:28', '', '', '', '', 'client/photos/animal_pic_057.jpg', 8),
+('2022-09-15 06:15:39', '', '', '', '', 'client/photos/animal_pic_058.jpg', 5),
+('2022-06-15 20:51:03', '', '', '', '', 'client/photos/animal_pic_059.jpg', 5),
+('2021-04-10 18:08:27', '', '', '', '', 'client/photos/animal_pic_060.jpg', 4),
+('2023-04-26 09:45:54', '', '', '', '', 'client/photos/animal_pic_061.jpg', 1),
+('2021-09-27 04:18:05', '', '', '', '', 'client/photos/animal_pic_062.jpg', 7),
+('2022-07-29 07:29:43', '', '', '', '', 'client/photos/animal_pic_063.jpg', 1),
+('2022-11-02 15:33:16', '', '', '', '', 'client/photos/animal_pic_064.jpg', 2),
+('2022-03-11 22:45:59', '', '', '', '', 'client/photos/animal_pic_065.jpg', 5),
+('2021-06-01 10:29:40', '', '', '', '', 'client/photos/animal_pic_066.jpg', 3),
+('2023-02-22 20:08:45', '', '', '', '', 'client/photos/animal_pic_067.jpg', 1),
+('2021-12-18 03:05:28', '', '', '', '', 'client/photos/animal_pic_068.jpg', 2),
+('2022-04-25 09:14:32', '', '', '', '', 'client/photos/animal_pic_069.jpg', 6),
+('2022-10-19 19:37:17', '', '', '', '', 'client/photos/animal_pic_070.jpg', 5),
+('2021-08-30 16:48:02', '', '', '', '', 'client/photos/animal_pic_071.jpg', 3),
+('2023-01-15 11:53:18', '', '', '', '', 'client/photos/animal_pic_072.jpg', 2),
+('2021-03-16 21:06:51', '', '', '', '', 'client/photos/animal_pic_073.jpg', 6),
+('2022-02-07 06:24:10', '', '', '', '', 'client/photos/animal_pic_074.jpg', 2),
+('2022-08-23 11:15:38', '', '', '', '', 'client/photos/animal_pic_075.jpg', 3),
+('2021-05-11 13:58:46', '', '', '', '', 'client/photos/animal_pic_076.jpg', 6),
+('2023-04-03 02:27:09', '', '', '', '', 'client/photos/animal_pic_077.jpg', 3),
+('2022-01-21 12:44:07', '', '', '', '', 'client/photos/animal_pic_078.jpg', 8),
+('2021-07-02 08:34:29', '', '', '', '', 'client/photos/animal_pic_079.jpg', 6),
+('2022-12-03 04:47:51', '', '', '', '', 'client/photos/animal_pic_080.jpg', 10),
+('2021-10-08 17:39:34', '', '', '', '', 'client/photos/animal_pic_081.jpg', 1),
+('2022-06-21 03:56:59', '', '', '', '', 'client/photos/animal_pic_082.jpg', 2),
+('2023-03-14 10:29:37', '', '', '', '', 'client/photos/animal_pic_083.jpg', 3),
+('2021-12-29 01:22:44', '', '', '', '', 'client/photos/animal_pic_084.jpg', 7),
+('2021-08-03 22:19:53', '', '', '', '', 'client/photos/animal_pic_085.jpg', 9),
+('2022-04-15 15:53:25', '', '', '', '', 'client/photos/animal_pic_086.jpg', 9),
+('2022-11-30 18:03:48', '', '', '', '', 'client/photos/animal_pic_087.jpg', 4),
+('2022-05-26 07:40:55', '', '', '', '', 'client/photos/animal_pic_088.jpg', 2)
