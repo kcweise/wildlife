d50503226c84085a19f1ced38ea5c3e5d3ed6be6
Keenan Weise <kcweise@gmail.com>
2024-05-18 08:25:35 -0500

    Prelim setup App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,35 +1,36 @@
+import { Route, Routes } from "react-router-dom";v
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from './vite.svg'
+import NavBar from "./components/NavBar";
+import HomePage from "./pages/HomePage";
+import AboutPage from "./pages/AboutPage";
+import LoginForm from "./components/LoginForm";
+import RegistrationForm from "./components/RegistrationForm";
+import UserDetail from "./components/UserDetail";
+import UserContext from "./UserContext";
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [isLogin, setIsLogin] = useState(false); // State to store the login status
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <UserContext.Provider
+        value={{ isLogin, setIsLogin }}
+      >
+        <div className="app">
+          <NavBar />
+          <Routes>
+            <Route path="/" element={<HomePage />} />
+            <Route path="/about" element={<AboutPage />} />
+            <Route path="/login" element={<LoginForm />} />
+            <Route path="/register" element={<RegistrationForm />} />
+            <Route path="/user/:id" element={<UserDetail />} />
+            <Route path="*" element={<NotFoundPage />} />
+          </Routes>
+        </div>
+      </UserContext.Provider>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
