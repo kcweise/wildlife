ef37e0923161be9656426c2679ecc4ee36c52f86
Keenan Weise <kcweise@gmail.com>
2024-05-20 10:11:39 -0500

    Added login form

--- client/src/components/LoginForm.jsx
+++ client/src/components/LoginForm.jsx
@@ -1,12 +1,92 @@
-import React from 'react';
+import React, { useState } from 'react';
+import {TextField, Button, Container, Typography, Box} from '@mui/material';
+
 
 function LoginForm() {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleLogin = async (e)=>{
+    e.preventDefault();
+    
+    try {
+      const response = await fetch('http://localhost:5555/login', {
+        method:'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ username, password }),
+        credentials: 'include',
+      });
+      
+      if (response.ok) {
+        const data = await response.json();
+        console.log('Login Successful:', data);
+      }
+      else {
+      console.error('Login failed', response.statusText);
+      }
+    }
+      catch (error) {
+      console.error('Error during login', error);
+    }   
+  };
+
+
+
   return (
-    <div>
-      <h1>Login Form</h1>
-      <p>Please log in to continue.</p>
-    </div>
+    <Container component="main" maxWidth="xs">
+      <Box
+        sx={{
+          marginTop: 8,
+          display: 'flex',
+          flexDirection: 'column',
+          alignItems: 'center',
+        }}
+      >
+        <Typography component="h1" variant="h5">
+          Login
+        </Typography>
+        <Box component="form" onSubmit={handleLogin} sx={{ mt: 1 }}>
+          <TextField
+            variant="outlined"
+            margin="normal"
+            required
+            fullWidth
+            id="username"
+            label="Username"
+            name="username"
+            autoComplete="username"
+            autoFocus
+            value={username}
+            onChange={(e) => setUsername(e.target.value)}
+          />
+          <TextField
+            variant="outlined"
+            margin="normal"
+            required
+            fullWidth
+            name="password"
+            label="Password"
+            type="password"
+            id="password"
+            autoComplete="current-password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+          <Button
+            type="submit"
+            fullWidth
+            variant="contained"
+            color="primary"
+            sx={{ mt: 3, mb: 2 }}
+          >
+            Login
+          </Button>
+        </Box>
+      </Box>
+    </Container>
   );
-}
+};
 
 export default LoginForm;
